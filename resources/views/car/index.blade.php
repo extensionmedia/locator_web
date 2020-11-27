e59c844fa98cc1ffcf77a5c433db509609b81b76
extensionmedia <elmeftouhi@gmail.com>
2020-11-27 10:23:28 +0100

    Grid cars design

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -31,17 +31,46 @@
         </div>
     </div>
 
-    <div class="grid grid-cols-4 gap-4 px-4">
-        @for ($i = 0; $i < 11; $i++)
-           <div class="col-span-1 card rounded-xl m-0 shadow-md relative">
-                <div class="absolute top-0 right-0 p-2 bg-red-700 rounded-full m-2"></div>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4 px-4">
+        @foreach ($cars as $car)
+           <a href="" class="col-span-1 card pb-0 rounded-xl m-0 shadow-md relative cursor-pointer hover:shadow-lg 
+           transition duration-500 ease-in-out transform hover:-translate-y-2 hover:scale-100">
+                <div class="absolute shadow-lg border top-0 right-0 p-2 rounded-full m-4" style="background-color: {{ $car['color'] }}"></div>
                 <div class="bg-gray-800 text-gray-100 rounded-lg py-1 px-2 text-xs inline">
-                    145874-A-40
+                    {{ $car['matricule'] }}
                 </div>
-                <h1 class="text-xl text-gray-600">Honda Jazz</h1>
-                <img class="object-cover w-16 h-16 rounded-full cursor-pointer" src="https://th.bing.com/th/id/OIP.wiGLjnZOQxFr2eV4I0-mXAHaEd?w=310&amp;h=186&amp;c=7&amp;o=5&amp;pid=1.7">
-            </div> 
-        @endfor
+
+                <img class="object-cover w-16 h-16 mx-auto rounded-full cursor-pointer my-2" src="{{ $car['image'] }}">
+                
+                <div class="text-center text-gray-800 text-sm">
+                    <h1 class="inline">{{ $car['marque'] }}</h1>
+                    <span class="text-gray-600 text-xs">{{ $car['serie'] }}</span>  
+                    <h1 class="">{{ $car['carburant'] }}</h1>                  
+                </div>
+                @if ($car['status'] == 1)
+                    <div class="my-2 py-1 text-xs bg-green-200 text-green-700 border-green-400 text-center rounded-xl border mx-auto w-20">
+                        Disponible
+                    </div>   
+                @elseif ($car['status'] == 0)
+                    <div class="my-2 py-1 text-xs bg-red-200 text-red-700 border-red-400 text-center rounded-xl border mx-auto w-20">
+                        Location
+                    </div>                 
+                @else
+                    <div class="my-2 py-1 text-xs bg-gray-200 text-gray-700 border-gray-400 text-center rounded-xl border mx-auto w-20">
+                        En Panne
+                    </div> 
+                @endif
+
+                <div class="py-1 border-b"></div>
+
+                <div class="flex items-center justify-between py-4">
+                    <div class="text-xs text-gray-400">
+                        <i class="far fa-calendar-alt"></i> Nov. 15, 2019
+                    </div>
+                </div>
+
+            </a>             
+        @endforeach
         
     </div>
 
