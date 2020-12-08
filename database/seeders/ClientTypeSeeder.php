3aa8d5a2fb90c2c90afe84fd5dfa867276eb7d89
extensionmedia <elmeftouhi@gmail.com>
2020-12-08 12:02:34 +0100

    Seeder

---
+++ database/seeders/ClientTypeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class ClientTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('client_type')->insert([
+            [
+                'client_type'   => 'Particulier',
+                'is_default'    =>  1               
+            ],
+            [
+                'client_type'   => 'Société',
+                'is_default'    =>  0                
+            ],
+            [
+                'client_type'   => 'Autres',
+                'is_default'    =>  0                
+            ]
+        ]);
+    }
+}
