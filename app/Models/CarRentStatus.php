2c3de735721104bfdc1667d033e5b854b077d320
Yassine <yassine.e@flowdesignssolutions.com>
2020-12-09 17:53:39 +0100

    Car Rent 3

--- app/Models/CarRentStatus.php
+++ app/Models/CarRentStatus.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CarRent extends Model{
+class CarRentStatus extends Model
+{
     use HasFactory;
-
-    public function status(){
-        return $this->hasOne(CarRentStatus::class, 'id','car_rent_status_id');
-    }
 }
